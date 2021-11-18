f848f72dc9c5c9b22623911b47d16b0112765922
Hannu <hanranti@users.noreply.github.com>
2021-11-18 12:45:35 +0200

    4.5

--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -4,7 +4,10 @@ const dummy = (blogs) => {
 
 const totalLikes = blogs => blogs.reduce((total, currentBlog) => total + currentBlog.likes, 0)
 
+const favoriteBlog = blogs => blogs.length > 0 ? blogs[blogs.map(blog => blog.likes).indexOf(Math.max(...blogs.map(blog => blog.likes)))] : null
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
