7e26e419d96dbd00220de865176725a7e69fbba8
Hannu <hanranti@users.noreply.github.com>
2021-11-18 14:30:11 +0200

    4.7

--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -8,17 +8,22 @@ const totalLikes = blogs => blogs.reduce((total, currentBlog) => total + current
 
 const favoriteBlog = blogs => blogs[blogs.map(blog => blog.likes).indexOf(Math.max(...blogs.map(blog => blog.likes)))]
 
-const blogAmountsByAuthor = blogs => blogs.reduce((allAmounts, currentBlog) => {
-    allAmounts.find(authorBlogs => authorBlogs.author === currentBlog.author)['blogs']++
+const blogDetailsByAuthor = blogs => blogs.reduce((allAmounts, currentBlog) => {
+    let authorDetails = allAmounts.find(authorBlogs => authorBlogs.author === currentBlog.author)
+    authorDetails['blogs']++
+    authorDetails['likes'] += currentBlog.likes
     return allAmounts
 },
- _.uniq(blogs.map(blog => blog.author)).map(author => ({author: author, blogs: 0})))
+ _.uniq(blogs.map(blog => blog.author)).map(author => ({author: author, blogs: 0, likes: 0})))
 
-const mostBlogs = blogs => _.maxBy(blogAmountsByAuthor(blogs), authorBlogs => authorBlogs.blogs)
+const mostBlogs = blogs => _.maxBy(blogDetailsByAuthor(blogs), authorBlogs => authorBlogs.blogs)
+
+const mostLikes = blogs => _.maxBy(blogDetailsByAuthor(blogs), authorBlogs => authorBlogs.likes)
 
 module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
-    mostBlogs
+    mostBlogs,
+    mostLikes
 }
