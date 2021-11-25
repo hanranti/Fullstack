d1cf694fb7b57b529d5af051cb1712a4882396e7
Hannu <hanranti@users.noreply.github.com>
2021-11-25 10:37:46 +0200

    4.8 and Refactor scripts

--- osa4/bloglist/index.js
+++ osa4/bloglist/index.js
@@ -1,6 +1,7 @@
 const config = require('./utils/config')
 const http = require('http')
 const app = require('./app')
+process.title = 'bloglist'
 const PORT = config.PORT || 3003
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
